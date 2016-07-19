ae7b73de3bdf6d8ef41f5f738474aa0a0c5766ac
wkd <shzwkd@foxmail.com>
2016-07-19 20:52:41 +0800

    导航页面修改

---
+++ demos/pages/ife/scripts/getLists.js
@@ -0,0 +1,20 @@
+/**
+ * Created by wangkaidong on 16/7/18.
+ */
+window.onload = function () {
+    var arr = [
+        ['../task0102.html', '任务二:零基础HTML及CSS编码(一)', 'http://ife.baidu.com/task/detail?taskId=2'],
+        ['../task0103.html', '任务三:三栏式布局', 'http://ife.baidu.com/task/detail?taskId=3'],
+        ['../task0104.html', '任务四:定位和居中问题', 'http://ife.baidu.com/task/detail?taskId=4'],
+        ['../task0105.html', '任务五:零基础HTML及CSS编码(二)', 'http://ife.baidu.com/task/detail?taskId=5'],
+        ['../task0108.html', '任务八:响应式网格(栅格化布局)', 'http://ife.baidu.com/task/detail?taskId=5']
+    ];
+    var lists = "";
+    for (var i = 0; i < arr.length; i++) {
+        lists += '<li class="task"><div class="taskInfo"><a href="' + arr[i][0] + '" class="taskDemoLink"><div class="taskTitle">' + arr[i][1] +
+            '</div></a><a href="' + arr[i][2] + '" class="taskLink"><div class="taskDetail">任务说明</div></a></div></li>';
+    }
+    lists = '<ul class="taskLists">' + lists + '</ul>';
+    document.getElementById('lists').innerHTML = lists;
+    console.log('list:' + lists);
+};
--- getMenu.js
+++ getMenu.js
@@ -3,17 +3,17 @@
  */
 //于数组中添加项目目录，格式为   ['URL', '名称']
 var arr  = [
-    ['单页面制作',
+    ['百度前端学院2016任务',
         ['demos/pages/ife/task0102.html','HTML+CSS简单页面'],
         ['demos/pages/ife/task0103.html','三栏式布局（左右固定中间自适应）'],
         ['demos/pages/ife/task0104.html','居中与定位'],
-        ['demos/pages/ife/task0105.html','简单页面+侧边栏'],
-        ['demos/pages/freeCodeCamp/tributePage.html','简介页面'],
-        ['demos/pages/ife/task0108.html','栅格化布局'],
-        ['demos/pages/study/boxsizingTest.html','box-sizing属性demo']
+        ['demos/pages/ife/index.html','More……']
     ],
-    ['网页小插件',
-        ['demos/pages/ife/task0201.html','简单事件绑定']
+    ['freeCodeCamp练手Demo',
+        ['demos/pages/freeCodeCamp/tributePage.html','简介页面']
+    ],
+    ['实践验证Demo',
+        ['demos/pages/study/boxsizingTest.html','box-sizing属性demo']
     ]
 ];
 var list = "";
@@ -22,7 +22,7 @@ for (var i = 0; i < arr.length; i++) {
         if (j == 0) {
             list += '<div class="myMenu"><dt class="fMenu">' + arr[i][0] + '</dt><div class="sMenu">';
         } else {
-            list += '<dd><a href="' + arr[i][j][0] + '" target="_blank">' + j + ")  " + arr[i][j][1] + '</a></dd>';
+            list += '<dd><a href="' + arr[i][j][0] + '" target="_blank">' + arr[i][j][1] + '</a></dd>';
         }
     }
     list += '</div></div>';
